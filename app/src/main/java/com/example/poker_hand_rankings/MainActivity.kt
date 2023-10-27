e53e3fd3ce3cfe67942b82516a7375be6f34e979
kimgunwooo <kwforu123@naver.com>
2023-10-27 13:08:07 +0900

    [ADD] main 화면 설정

--- app/src/main/java/com/example/poker_hand_rankings/MainActivity.kt
+++ app/src/main/java/com/example/poker_hand_rankings/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.poker_hand_rankings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.poker_hand_rankings.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var main : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        // 바인딩 처리
+        main = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(main.root)
+
+        // main 이미지 설정
+        main.card1.setImageResource(R.drawable.c_king_of_hearts2)
     }
 }
