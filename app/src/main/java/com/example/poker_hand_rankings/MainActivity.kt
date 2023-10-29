77a63b2ef6521ad50f2a0bb4d13ea627c5ced320
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-10-29 14:59:16 +0900

    [ADD] main OnClickListener로 통합

--- app/src/main/java/com/example/poker_hand_rankings/MainActivity.kt
+++ app/src/main/java/com/example/poker_hand_rankings/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.poker_hand_rankings
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
 import com.example.poker_hand_rankings.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener{
     private lateinit var main : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,16 +19,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(main.root)
 
         // main 이미지 설정
-        main.card1.setImageResource(R.drawable.c_king_of_hearts2)
+        main.card1.setImageResource(R.drawable.c_red_joker)
 
-        main.btnRandom.setOnClickListener{
-            val intent = Intent(this, RandomActivity::class.java)
-            startActivity(intent)
-        }
+        main.btnRandom.setOnClickListener(this)
+        main.btnStatistics.setOnClickListener(this)
+    }
 
-        main.btnStatistics.setOnClickListener{
-            val intent = Intent(this, StatisticsActivity::class.java)
-            startActivity(intent)
+    override fun onClick(p0: View?) {
+        val toClass = when (p0?.id){
+            main.btnRandom.id -> { RandomActivity::class.java }
+            main.btnStatistics.id -> { StatisticsActivity::class.java }
+            else -> return
         }
+        val intent = Intent(this,toClass)
+        startActivity(intent)
     }
 }
